0f7ec97c10baa71d8c8a4aa5943e49d9a4abcf82
qiankeqin <qiankeqin@51xf.cn>
2018-12-29 17:30:53 +0800

    first

---
+++ src/main/java/xyz/spring4all/springbootdroolstest/SpringBootDroolsTestApplication.java
@@ -0,0 +1,14 @@
+package xyz.spring4all.springbootdroolstest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootDroolsTestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootDroolsTestApplication.class, args);
+	}
+
+}
+
