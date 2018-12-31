afa0794bfffd5f1bd4b12733ad7ade5b866a2efd
qiankeqin <qiankeqin@51xf.cn>
2018-12-31 11:28:53 +0800

    insert update

---
+++ src/main/java/xyz/spring4all/springbootdroolstest/domain/Student.java
@@ -0,0 +1,10 @@
+package xyz.spring4all.springbootdroolstest.domain;
+
+/**
+ * @program: spring-boot-drools-test
+ * @description:
+ * @author: qiankeqin
+ * @create: 2018-12-31 10:37
+ **/
+public class Student {
+}
