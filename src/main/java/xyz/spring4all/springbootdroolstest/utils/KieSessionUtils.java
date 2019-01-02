553a443bc4d970e94d4ef57238e2939140c54543
qiankeqin <qiankeqin@51xf.cn>
2019-01-02 09:46:23 +0800

    修改KieSessionUtils

--- src/main/java/xyz/spring4all/springbootdroolstest/utils/KieSessionUtils.java
+++ src/main/java/xyz/spring4all/springbootdroolstest/utils/KieSessionUtils.java
@@ -1,9 +1,23 @@
 package xyz.spring4all.springbootdroolstest.utils;
 
+import jdk.internal.util.xml.impl.Input;
+import org.drools.decisiontable.InputType;
+import org.drools.decisiontable.SpreadsheetCompiler;
+import org.kie.api.KieBase;
 import org.kie.api.KieServices;
 import org.kie.api.builder.*;
+import org.kie.api.io.Resource;
+import org.kie.api.io.ResourceType;
+import org.kie.api.runtime.KieContainer;
 import org.kie.api.runtime.KieSession;
+import org.kie.internal.utils.KieHelper;
 import org.springframework.core.io.support.PathMatchingResourcePatternResolver;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.InputStream;
+import java.util.List;
 
 /**
  * @program: spring-boot-drools-test
@@ -48,13 +62,79 @@ public class KieSessionUtils {
 
     /**
      * 快速新建KieSession
-     * @param classPath 绝对露肩
+     * @param classPath 绝对路径
      * @return KieSession有状态
      * @throws Exception
      */
     public static KieSession newKieSession(String classPath) throws Exception{
-//        KieSession kieSession = getKieBase(classPath)
+        KieSession kieSession = getKieBase(classPath).newKieSession();
         return kieSession;
     }
+
+    /**
+     * 清空对象
+     */
+    public static void disposeKieSession(){
+        if(kieSession!=null){
+            kieSession.dispose();
+            kieSession = null;
+        }
+    }
+
+    protected static KieBase getKieBase(String classpath) throws Exception {
+        KieServices kieServices = KieServices.Factory.get();
+        KieFileSystem kfs = kieServices.newKieFileSystem();
+        Resource resource = kieServices.getResources().newClassPathResource(classpath);
+        resource.setResourceType(ResourceType.DRL);
+        kfs.write(resource);
+        KieBuilder kieBuilder = kieServices.newKieBuilder(kfs).buildAll();
+        if(kieBuilder.getResults().getMessages(Message.Level.ERROR).size()>0){
+            throw new Exception();
+        }
+        KieContainer kieContainer = kieServices.newKieContainer(kieServices.getRepository().getDefaultReleaseId());
+        KieBase kieBase = kieContainer.getKieBase();
+        return kieBase;
+    }
+
+    /**
+     * 根据服务器真实路径下的xls文件生成drl文件内容
+     * @param realPath
+     * @return
+     * @throws FileNotFoundException
+     */
+    public static KieSession getKieSessionFromXls(String realPath) throws FileNotFoundException {
+        return createKieSessionFromXls(getDRL(realPath));
+    }
+
+    /**
+     * 将drl内容转化为session
+     * @param drl
+     * @return
+     */
+    public static KieSession createKieSessionFromXls(String drl) {
+        KieHelper kieHelper = new KieHelper();
+        kieHelper.addContent(drl,ResourceType.DRL);
+        Results results = kieHelper.verify();
+        if(results.hasMessages(Message.Level.WARNING,Message.Level.ERROR)){
+            List<Message> messages = results.getMessages(Message.Level.WARNING, Message.Level.ERROR);
+            messages.forEach(System.out::println);
+            throw new IllegalStateException("compilation errors were found,check the logs");
+        }
+        return kieHelper.build().newKieSession();
+    }
+
+
+    /**
+     * 把xls解析成String
+     * @param realPath
+     * @return
+     */
+    public static String getDRL(String realPath) throws FileNotFoundException {
+        File file = new File(realPath);
+        InputStream inputStream = new FileInputStream(file);
+        SpreadsheetCompiler spreadsheetCompiler = new SpreadsheetCompiler();
+        return spreadsheetCompiler.compile(inputStream, InputType.XLS);
+    }
+
 
 }
